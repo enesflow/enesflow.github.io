d8923c012925e0cbcd67cd0474cb9dc5eab776af
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-10-01 17:22:13 +0300

    Update main.js

--- home/main.js
+++ home/main.js
@@ -1,6 +1,6 @@
 const loader = document.getElementById("loader");
 
-var projects = document.getElementsByClassName("project");
+var projects = document.getElementsByClassName("img");
 for (var i = 0; i < projects.length; i++) {
   projects[i].addEventListener("click", function(e) {
       loader.style.display = "inline-block";
