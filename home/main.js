346cbfb83fbbd18abffb2404044d7dcd3a1522a1
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-10-01 17:19:03 +0300

    Update main.js

--- home/main.js
+++ home/main.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const loader = document.getElementById("loader");
+
+var projects = document.getElementsByClassName("project");
+for (var i = 0; i < projects.length; i++) {
+  projects[i].addEventListener("click", function(e) {
+      loader.style.display = "inline-block";
+  });
+}
