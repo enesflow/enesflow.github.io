d4d22a1173c9d410579f7bd19834df7f956a6b00
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-10-01 17:29:20 +0300

    Update main.js

--- home/main.js
+++ home/main.js
@@ -1,4 +1,5 @@
 const loader = document.getElementById("loader");
+
 var projects = document.getElementsByClassName("img");
 for (var i = 0; i < projects.length; i++) {
   projects[i].addEventListener("click", function(e) {
