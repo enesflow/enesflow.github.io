ca1f063771ec7ff2229766215feb4755e0521392
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-10-01 17:30:12 +0300

    Create main.js

---
+++ home/main.js
@@ -0,0 +1,8 @@
+const loader = document.getElementById("loader");
+
+var projects = document.getElementsByClassName("img");
+for (var i = 0; i < projects.length; i++) {
+  projects[i].addEventListener("click", function(e) {
+      loader.style.display = "inline-block";
+  });
+}
