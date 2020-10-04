1f24742b1c7b44f6755d39b2e047a7a81f03c516
EnxGitHub <65719051+EnxGitHub@users.noreply.github.com>
2020-10-04 12:43:23 +0300

    Update main.js

--- home/main.js
+++ home/main.js
@@ -2,7 +2,10 @@ const loader = document.getElementById("loader");
 
 var projects = document.getElementsByClassName("img");
 for (var i = 0; i < projects.length; i++) {
-  projects[i].addEventListener("click", function(e) {
-      loader.style.display = "inline-block";
-  });
+    projects[i].addEventListener("click", function(e) {
+        loader.style.display = "inline-block";
+        for (var j = 0; j < projects.length; j++) {
+            projects[j].style.pointerEvents = "none";
+        }
+    });
 }
