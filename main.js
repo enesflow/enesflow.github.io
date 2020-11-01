ee2841ef151ea674b965090b615f4617cacbde23
EnxGitHub <eneskavcakar34@gmail.com>
2020-11-01 22:30:09 +0300

    Version 2
    Version 2

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
-const loader = document.getElementById("loader");
-
-var projects = document.getElementsByClassName("img");
-for (var i = 0; i < projects.length; i++) {
-    projects[i].addEventListener("click", function(e) {
-        loader.style.display = "inline-block";
-        for (var j = 0; j < projects.length; j++) {
-            projects[j].style.pointerEvents = "none";
-        }
-    });
+if (window.location.href.includes("github.io")) {
+    document.getElementById("github").style.display = "none";
 }
-=======
+if (window.location.href.includes("netlify.app")) {
+    document.getElementById("netlify").style.display = "none";
+}
+if (window.location.href.includes("web.app")) {
+    document.getElementById("firebase").style.display = "none";
+}
+
 //Materialize CSS navbar thing
 document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll(".sidenav");
@@ -22,4 +19,3 @@ document.addEventListener("DOMContentLoaded", function () {
 $(document).ready(function () {
     $(".sidenav").sidenav();
 });
->>>>>>> 6ecdaf3 (2.0 version)
