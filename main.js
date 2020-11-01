04b0b6613293b257e7992c58b7802364834c23b7
EnxGitHub <eneskavcakar34@gmail.com>
2020-11-01 21:47:15 +0300

    version 2.0
    The next version with Materialize CSS

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const loader = document.getElementById("loader");
 
 var projects = document.getElementsByClassName("img");
@@ -9,3 +10,16 @@ for (var i = 0; i < projects.length; i++) {
         }
     });
 }
+=======
+//Materialize CSS navbar thing
+document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $(".sidenav").sidenav();
+});
+>>>>>>> 6ecdaf3 (2.0 version)
